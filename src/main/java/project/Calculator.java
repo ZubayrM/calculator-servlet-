cda46d88f3462044a35b75eb73e81b524b8cc3a0
zubayr_@live.com <reflenie1roliK>
2020-08-31 21:58:49 +0300

    31/08/2020

---
+++ src/main/java/project/Calculator.java
@@ -0,0 +1,88 @@
+package project;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+
+@WebServlet("/")
+public class Calculator extends HttpServlet {
+
+    private Pattern minus = Pattern.compile("\\d+\\s\\u002D\\s\\d+");
+    private Pattern plus = Pattern.compile("\\d+\\s\\u002B\\s\\d+");
+    private Pattern divide = Pattern.compile("\\d+\\s\\u002F\\s\\d+");
+    private Pattern multiply = Pattern.compile("\\d+\\s\\u002A\\s\\d+");
+
+    private String numbers = "";
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("myText", numbers);
+        req.getRequestDispatcher("calculator.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setCharacterEncoding("UTF-8");
+        String button = req.getParameter("button");
+
+        switch (button){
+            case "C":
+                numbers = "";
+                break;
+            case "delete":
+                numbers = numbers.substring(0, numbers.length() > 0 ? numbers.length()-1 : 0);
+                break;
+            case "+\u002F-":
+                numbers = numbers.startsWith("-") ? numbers.substring(1) : "-".concat(numbers);
+            case "=":
+
+                numbers = numbers.replace(",", ".");
+                String[] s = numbers.split("\\s");
+                double numOne = Double.parseDouble(s[0]);
+                double numTwo = Double.parseDouble(s[2]);
+                double result = getResultArithmetic(numOne, numTwo);
+                numbers = String.valueOf(result);
+                numbers = numbers.replace(".", ",");
+                break;
+
+
+            default:
+
+                numbers += button;
+
+        }
+
+
+
+        req.setAttribute("myText", numbers);
+        req.getRequestDispatcher("calculator.jsp").forward(req, resp);
+    }
+
+    private double getResultArithmetic(double numOne, double numTwo ) {
+        double result = 0;
+        if (minus.matcher(numbers).find()){
+            result = (numOne - numTwo);
+        }
+        else if (plus.matcher(numbers).find()){
+            result = (numOne + numTwo);
+        }
+        else if (divide.matcher(numbers).find()){
+            result = (numOne / numTwo);
+        }
+        else if (multiply.matcher(numbers).find()){
+            result = (numOne* numTwo);
+        }
+        return result;
+    }
+}
---
+++ src/main/webapp/calculator.jsp
@@ -0,0 +1,57 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+
+<%--    <link rel="stylesheet" type="text/css" href="style.css"/>--%>
+    <style>
+        <%@ include file="style.css" %>
+    </style>
+
+    <h1>${count}</h1>
+
+    <title>calculator</title>
+</head>
+<body>
+<p> <h1>${time}</h1> </p>
+<form action="/calculator_war/" method="post">
+    <table class="cal" border="0">
+        <tr class="screen">
+            <td colspan = "4"><input type="text" name="text" disabled="true" value="${myText}" style="height: 40px; font-size: 25px; width: 100%; text-align: right;" ></td>
+        </tr>
+        <tr class="button">
+            <td>java</td>
+            <td><button name="button" value="C">C</button></td>
+            <td><button name="button" value="delete">&#9003;</button></td>
+            <td><button name="button" value=" / ">&#247;</button></td>
+        </tr>
+        <tr class="button">
+            <td class="number"><button name="button" value="7">7</button></td>
+            <td class="number"><button name="button"  value="8">8</button></td>
+            <td class="number"><button name="button" value="9">9</button></td>
+            <td><button name="button" value=" * ">*</button></td>
+        </tr>
+        <tr class="button">
+            <td class="number"><button name="button" value="4">4</button></td>
+            <td class="number"><button name="button" value="5">5</button></td>
+            <td class="number"><button name="button" value="6">6</button></td>
+            <td><button name="button" value=" - ">-</button></td>
+        </tr>
+        <tr class="button">
+            <td class="number"><button name="button" value="1">1</button></td>
+            <td class="number"><button name="button" value="2">2</button></td>
+            <td class="number"><button name="button" value="3">3</button></td>
+            <td><button name="button" value=" + ">+</button></td>
+        </tr>
+        <tr class="button">
+            <td><button name="button" value="+/-">+/-</button></td>
+            <td class="number"><button name="button" value="0">0</button></td>
+            <td><button name="button" value=",">,</button></td>
+            <td><button name="button" value="=">=</button></td>
+        </tr>
+
+    </table>
+
+</form>
+
+</body>
+</html>
